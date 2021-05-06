94ab3cf1349a45177a0e035c3877aac50c1f24bd
josejavierpaez <josejavierpaez@hotmail.com>
2021-05-06 10:48:05 -0400

    🎉 init(setup): project setup

---
+++ src/handler/updateTodo.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const datetime = new Date().toISOString();
+module.exports.updateTodo = (event, context, callback) => {
+	const data = JSON.parse(event.body);
+
+	if (typeof data.task !== 'string' || typeof data.done !== 'boolean') {
+		console.error('Value of task or done is invalid');
+		const response = {
+			statusCode: 400,
+			body: JSON.stringify({ message: 'Value of task or done is invalid' }),
+		};
+		return;
+	}
+	const params = {
+		TableName: 'TodoListTable',
+		Key: {
+			id: event.pathParameters.id,
+		},
+		ExpressionAttributeValues: {
+			':t': data.task,
+			':d': data.done,
+			':u': datetime,
+		},
+		UpdateExpression: 'set task = :t, done = :d, updateAt = :u',
+	};
+
+	dynamoDb.update(params, (error, data) => {
+		if (error) {
+			console.error(error);
+			callback(new Error(error));
+			return;
+		}
+
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify(data.Item),
+		};
+
+		callback(null, response);
+	});
+};
